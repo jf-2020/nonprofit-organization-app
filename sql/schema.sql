6d9440a6df3773d8e2cdc5073036b4207eaf4639
Liz Aitken <liz.a.aitken@gmail.com>
2019-06-14 22:28:10 -0400

    Added data

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,93 +1,78 @@
-/* TODO: create the following tables:
- *			-grade 		-> DONE
- *			-supplies	-> DONE
- *			-schools 	-> DONE
- *			-ites 		-> HALF
- *			-stores 	-> DONE
- *			-students 	-> DONE
- *			-families 	-> DONE
- *			-users 		-> DONE
- *			-sponsors 	-> DONE
- */
-
 CREATE TABLE users (
-	user_id 		SERIAL 			PRIMARY KEY	,
-	password		VARCHAR(100)				,
-	first_name 		VARCHAR(50)					,
-	last_name 		VARCHAR(50)					,
-	email 			VARCHAR(200)				,
-	phone 			VARCHAR(200)				,
-	photo 			VARCHAR(500)				,
-	user_type 		INT
+    user_id         SERIAL             PRIMARY KEY    ,
+    password        VARCHAR(100)                ,
+    first_name         VARCHAR(50)                    ,
+    last_name         VARCHAR(50)                    ,
+    email             VARCHAR(200)                ,
+    phone             VARCHAR(200)                ,
+    photo             VARCHAR(500)                ,
+    user_type         VARCHAR(30)
 );
 
 CREATE TABLE families (
-	family_id 				SERIAL 			PRIMARY KEY	,
-	name 					VARCHAR(50)					,
-	size 					INT							,
-	location 				VARCHAR(250)				,
-	money 					DECIMAL(8,2)				,
-	church_affiliation 		VARCHAR(100)
+    family_id                 SERIAL             PRIMARY KEY    ,
+    name                     VARCHAR(50)                    ,
+    size                     INT                            ,
+    location                 VARCHAR(250)                ,
+    money                     DECIMAL(8,2)                ,
+    church_affiliation         VARCHAR(100)
 );
 
 CREATE TABLE stores (
-	store_id 	SERIAL 			PRIMARY KEY	,
-	name 		VARCHAR(100)				,
-	location 	VARCHAR(250)
+    store_id     SERIAL             PRIMARY KEY    ,
+    name         VARCHAR(100)                ,
+    location     VARCHAR(250)
 );
 
 CREATE TABLE schools (
-	school_id 	INT 			PRIMARY KEY	,
-	name 		VARCHAR(250)				,
-	address 	VARCHAR(250)
-);
-
-CREATE TABLE items (
-	item_list_id	SERIAL	PRIMARY KEY
+    school_id     INT             PRIMARY KEY    ,
+    name         VARCHAR(250)                ,
+    address     VARCHAR(250)
 );
 
 CREATE TABLE grades (
-	grade_id 	SERIAL 			PRIMARY KEY						,
-	level 		VARCHAR(30)										,
-	items_id 	INT 			REFERENCES items (item_list_id)	,
-	school_id 	INT 			REFERENCES schools (school_id)
-);
-
-CREATE TABLE links (
-    link_id         SERIAL             PRIMARY KEY        ,
-    stores_id         INT                                ,
-    students_id     INT                                ,
-    families_id     INT
+    grade_id     SERIAL             PRIMARY KEY                        ,
+    level         VARCHAR(30)                                        ,
+    school_id     INT             REFERENCES schools (school_id)
 );
 
 CREATE TABLE sponsors (
-	sponsor_id 			SERIAL 			PRIMARY KEY							,
-	first_name 			VARCHAR(255)										,
-	last_name 			VARCHAR(255)										,
-	email 				VARCHAR(500)										,
-	phone 				VARCHAR(50)											,
-	money_donated 		DECIMAL(8,2)										,
-	date_paid 			DATE												,
-	date_deposited 		DATE												,
-	photo 				VARCHAR(200)										,
-	link_id				INT				REFERENCES links (link_id)
+    sponsor_id             SERIAL             PRIMARY KEY                            ,
+    first_name             VARCHAR(255)                                        ,
+    last_name             VARCHAR(255)                                        ,
+    email                 VARCHAR(500)                                        ,
+    phone                 VARCHAR(50)                                            ,
+    money_donated         DECIMAL(8,2)                                        ,
+    date_paid             VARCHAR(20)                                            ,
+    date_deposited         VARCHAR(20)                                            ,
+    photo                 VARCHAR(200)                                        ,
+    link_id                INT
 );
 
 CREATE TABLE students (
-	student_id 		SERIAL 			PRIMARY KEY							,
-	first_name 		VARCHAR(50)											,
-	last_name 		VARCHAR(50)											,
-	age 			INT													,				
-	sponsorship 	VARCHAR(30)											,
-	grades_id 		INT													,
-	money 			DECIMAL(8,2)										,
-	link_id 		INT 			REFERENCES links (link_id)
+    student_id         SERIAL             PRIMARY KEY                            ,
+    first_name         VARCHAR(50)                                            ,
+    last_name         VARCHAR(50)                                            ,
+    age             INT                                                    ,                
+    sponsorship     VARCHAR(30)                                            ,
+    grades_id         INT                                                    ,
+    money             DECIMAL(8,2)                                        
 );
 
 CREATE TABLE supplies (
-	supply_id 		SERIAL 			PRIMARY KEY						,
-	product_name 	VARCHAR(100)									,
-	price 			DECIMAL(8,2)									,
-	quantity 		INT												,
-	link_id 		INT 			REFERENCES links (link_id)
+    supply_id         SERIAL             PRIMARY KEY                         ,
+    supply_name     VARCHAR(100)                                     ,
+    unit_cost        NUMERIC(8,2)                                     ,
+    quantity         INT                                                 ,
+    student_id         INT                REFERENCES students (student_id) ,
+    store_id         INT                REFERENCES stores (store_id)      ,
+    grade_id         INT                REFERENCES grades (grade_id)
+);
+
+CREATE TABLE links (
+    link_id         SERIAL                PRIMARY KEY                            ,
+    stores_id       INT                                                     ,
+    students_id     INT                    REFERENCES students (student_id)    ,
+    families_id     INT                    REFERENCES families (family_id)        ,
+    sponsors_id        INT                    REFERENCES sponsors (sponsor_id)
 );
--- sql/update.sql
+++ sql/update.sql
@@ -6,17 +6,18 @@
 --REMOVES
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS sponsors;
-DROP TABLE IF EXISTS children;
+DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS families;
 DROP TABLE IF EXISTS stores;
 DROP TABLE IF EXISTS schools;
 DROP TABLE IF EXISTS supplies;
 DROP TABLE IF EXISTS grades;
-DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS links;
 DROP DATABASE nonprofit_organization_app;
 
 --CREATE & CONNECT
 CREATE DATABASE nonprofit_organization_app;
 \c nonprofit_organization_app;
 \i schema.sql;
-\i seed.sql;
+\i seed.sql
+
