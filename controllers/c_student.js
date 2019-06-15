7c8a62ff945893360fa3a9553b0fb305f4badcb3
Liz Aitken <liz.a.aitken@gmail.com>
2019-06-15 16:58:59 -0400

    Added functions in order to get info for student controller, view. Updated view for student list

--- controllers/c_student.js
+++ controllers/c_student.js
@@ -1,6 +1,8 @@
 const Students = require('../models/m_student');
 const Family = require('../models/m_family');
 const Supplies = require('../models/m_supplies');
+const Grades = require('../models/m_grade');
+const Sponsors = require('../models/m_sponsor');
 
 exports.getAllStudents = async (req, res) => {
     let arrOfStudents = await Students.getAllStudents();
@@ -23,7 +25,10 @@ exports.getAllStudents = async (req, res) => {
 exports.getOneStudent = async (req, res) => {
     const student_id = req.params.student;
     const arrOfStudents = await Students.getOneStudent(student_id);
+    const familyInfo = await Family.getOneFamily(student_id);
     const allSupplies = await Supplies.getAllItemsByStudentID(student_id);
+    const gradeInfo = await Grades.getOneGrade(student_id);
+    const sponsorInfo = await Sponsors.getSponsorById(student_id);
 
     res.render('template', {
         locals: {
@@ -31,7 +36,10 @@ exports.getOneStudent = async (req, res) => {
             userName: req.session.first_name,
             is_logged_in: req.session.is_logged_in,
             studentList: arrOfStudents,
-            suppliesList: allSupplies
+            familyName: familyInfo,
+            gradeLevel: gradeInfo,
+            suppliesList: allSupplies,
+            sponsorName: sponsorInfo
         },
         partials: {
             partial: 'partial-studentsProfile',
