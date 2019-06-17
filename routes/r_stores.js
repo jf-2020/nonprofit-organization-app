f21aa6f94c9092a42b6b45b88d89628beef28575
jf-2020 <jcf129er@gmail.com>
2019-06-16 20:40:11 -0400

    completed stores add/delete feature in full

--- routes/r_stores.js
+++ routes/r_stores.js
@@ -2,8 +2,20 @@ const express = require('express'),
       router = express.Router(),
       storeController = require('../controllers/c_stores');
 
+/* GET route for page for deleting a store */
+router.get('/delete-store', storeController.deleteStore_get);
+
+/* POST route for store deletion */
+router.post('/delete-store', storeController.deleteStore_post);
+
+/* GET store page for adding a store */
+router.get('/add-store', storeController.addStore_get);
+
+/* POST route for adding a store */
+router.post('/add-store', storeController.addStore_post);
+
 /* GET stores page */
-router.get('/', storeController.getAllStores);
 router.get('/:store', storeController.getOneStore);
+router.get('/', storeController.getAllStores);
 
 module.exports = router;
