306a37390eb60044cb1489c149a94c77b387a155
jf-2020 <jcf129er@gmail.com>
2019-06-17 21:47:32 -0400

    completed to-do list feature in full + a few minor changes throughout

--- controllers/c_todo.js
+++ controllers/c_todo.js
@@ -17,30 +17,49 @@ exports.getTodoList = async (req, res) => {
     });
 };
 
-exports.addToDoList = async function(req, res) {
-    const {todo_text, due_date, people_assigned, date_completed} = req.body;
-    console.log(people_assigned);
-    const arrOfTodo = await Todos.getAllTodo();
-    console.log("THis is arrrrrrr    :   ", arrOfTodo);
-    
-    const addNew = await Todos.addToDo(todo_text, due_date, people_assigned, date_completed);
-    console.log("THis is add New    :   ", addNew);
-    
-
+exports.addTodo_get = async (req, res) => {
     res.render('template', {
         locals: {
-            title: 'To-Do List',
+            title: 'Add To-Do',
             userName: req.session.first_name,
-            is_logged_in: req.session.is_logged_in,
-            todoList: arrOfTodo,
-            addMore: addNew
+            is_logged_in: req.session.is_logged_in
         },
         partials: {
-            partial: 'partial-todoList',
+            partial: 'partial-add-todo',
             nav: 'partial-nav'
         }
     });
 }
 
+exports.addTodo_post = async (req, res) => {
+    const {todo_text, due_date, people_assigned, date_completed} = req.body;
+    
+    Todos.addToDo(todo_text, due_date, people_assigned, date_completed)
+    .then(() => {
+        console.log("success!");
+        res.redirect('/to-do-list');
+    });
+}
 
+exports.removeTodo_get = async (req, res) => {
+    res.render('template', {
+        locals: {
+            title: 'Remove To-Do',
+            userName: req.session.first_name,
+            is_logged_in: req.session.is_logged_in
+        },
+        partials: {
+            partial: 'partial-remove-todo',
+            nav: 'partial-nav'
+        }
+    });
+}
 
+exports.removeTodo_post = async (req, res) => {
+    const { todo_text, people_assigned } = req.body;
+    const todo = new Todos(null, todo_text, null, people_assigned, null);
+
+    todo.deleteToDo().then(() => {
+        res.redirect('/to-do-list');
+    });
+}
